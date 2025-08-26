5e8a07bbaa5ceda56387ed7679618986b22278b0
Marcos M. Crocci <marcos.crocci@prestadores.amil.com.br>
2025-08-26 12:11:32 -0300

    feat: adiciona nova classe Main com exemplos de variáveis e impressão de dados

---
+++ Main_aula37.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte idade = 25;
+        short ano = 2024;
+        int populacaoCidade = 500000;
+        long populacaoMundial = 7800000000L;
+        float altura = 1.75f;
+        double salario = 4500.50;
+        boolean estudante = true;
+        char inicialNome = 'A';
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("População da cidade: "
+                + populacaoCidade);
+        System.out.println("População mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salário: " + salario);
+        System.out.println("É estudante? " + estudante);
+        System.out.println("Inicial do nome: " + inicialNome);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# curso_java
+# Curso de Java Completo
+
+### Ajustar a configuração do terminal para o encoding UTF-8 no CMD
+> chcp 65001
